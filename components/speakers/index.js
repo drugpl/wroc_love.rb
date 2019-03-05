1644d0ad0901f7a8395a09ff0531b2e5ddf21ce0
Michal Matyas <github@higher.lv>
2019-03-05 23:39:43 +0100

    Enable opening and closing speaker description

--- components/speakers/index.js
+++ components/speakers/index.js
@@ -1,37 +1,13 @@
-import React, { useState } from "react";
-import nl2br from "react-nl2br";
+import React from "react";
 import styles from "./index.scss";
 import SectionHeader from "../section_header";
+import Speaker from './speaker'
 
 const Speakers = ({ speakers }) => (
   <div className={styles.container}>
     <SectionHeader>Speakers</SectionHeader>
-    {speakers.map(({ image, name, description, links }, index) => (
-      <div className={styles.speaker} key={index} tabIndex="0">
-        <div className={styles.speaker_photo_container}>
-          <div
-            className={styles.speaker_photo}
-            style={{ backgroundImage: `url(${image})` }}
-          />
-        </div>
-        <div className={styles.speaker_details}>
-          <div className={styles.speaker_name}>{name}</div>
-          <div className={styles.speaker_description}>
-            {description.html ? (
-              <div dangerouslySetInnerHTML={{ __html: description.html }} />
-            ) : (
-              nl2br(description)
-            )}
-            {links && (
-              <div className={styles.speaker_links}>
-                {Object.entries(links).map(([name, link], index) => (
-                  <a href={link} key={index}>{`${name} `}</a>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
+    {speakers.map((speaker, index) => (
+      <Speaker {...speaker} index={index} />
     ))}
   </div>
 );
---
+++ components/speakers/speaker.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+import nl2br from "react-nl2br";
+import styles from "./speaker.scss";
+
+const Speaker = ({ image, name, description, links }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <div
+      className={`${styles.speaker} ${isOpen ? styles.speaker_open : ""}`}
+      onClick={() => setOpen(!isOpen)}
+    >
+      <div className={styles.speaker_photo_container}>
+        <div
+          className={styles.speaker_photo}
+          style={{ backgroundImage: `url(${image})` }}
+        />
+      </div>
+      <div className={styles.speaker_details}>
+        <div className={styles.speaker_name}>{name}</div>
+        <div className={styles.speaker_description}>
+          {description.html ? (
+            <div dangerouslySetInnerHTML={{ __html: description.html }} />
+          ) : (
+            nl2br(description)
+          )}
+          {links && (
+            <div className={styles.speaker_links}>
+              {Object.entries(links).map(([name, link], index) => (
+                <a href={link} key={index}>{`${name} `}</a>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Speaker.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.oneOfType(
+    PropTypes.shape({
+      __html: PropTypes.string
+    }),
+    PropTypes.string
+  ),
+  links: PropTypes.object
+};
+
+export default Speaker;
