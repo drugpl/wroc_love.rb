d16dcfd6cc65269815e96ab43a37f8a4797208f3
Michal Matyas <github@higher.lv>
2019-03-07 08:45:22 +0100

    Disable scrolling on map on mobile

--- components/location/venue_map.js
+++ components/location/venue_map.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import L from 'leaflet'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import './venue_map.scss'
 
 const VenueMap = ({ venue }) => (
-  <Map center={[venue.lat, venue.lng]} zoom={13}>
+  <Map center={[venue.lat, venue.lng]} zoom={13} dragging={!L.Browser.mobile}>
     <TileLayer
       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
