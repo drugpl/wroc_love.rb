372cabade7246014717933cec2aafb0e2dd3f4fb
Michal Matyas <github@higher.lv>
2019-03-12 15:23:37 +0100

    Fix https warning

--- components/location/venue_map.js
+++ components/location/venue_map.js
@@ -7,7 +7,7 @@ const VenueMap = ({ venue }) => (
   <Map center={[venue.lat, venue.lng]} zoom={13} dragging={!L.Browser.mobile}>
     <TileLayer
       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+      url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
     />
     <Marker position={[ venue.lat, venue.lng ]}>
       <Popup maxWidth={200}>
