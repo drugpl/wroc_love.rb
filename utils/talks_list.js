70e8ad2dbd86d380c49c930c8ac4740e1bc35bb4
Michal Matyas <github@higher.lv>
2019-03-24 07:43:31 +0100

    Fix Safari bug with incorrect Agenda dot placement

--- utils/talks_list.js
+++ utils/talks_list.js
@@ -1,3 +1,5 @@
+const zeroPad = value => String(value).length === 4 ? `0${value}` : String(value)
+const createDate = (date, time) => new Date(`${date}T${zeroPad(time)}:00+01:00`)
 export default function talksList(agenda) {
   if(!agenda) {
     return []
@@ -7,8 +9,8 @@ export default function talksList(agenda) {
     const { date, talks } = day
     talks.forEach(talk => {
       const { start, end, title, speaker } = talk
-      const startTime = new Date(`${date} ${start}:00`)
-      const endTime = new Date(`${date} ${end}:00`)
+      const startTime = createDate(date, start)
+      const endTime = createDate(date, end)
 
       list.push({
         date, start, end, title, speaker, startTime, endTime
