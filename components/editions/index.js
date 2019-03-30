c99aad494209b5f81a1545e5f57949597e59c949
Michal Matyas <github@higher.lv>
2019-03-30 15:49:27 +0100

    Add previous editions links

---
+++ components/editions/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./index.scss";
+import { withConfiguration } from "../contexts/configuration";
+
+const Editions = ({ editions }) => (
+  <div className={styles.container}>
+    <div className={styles.header}>Previously on wroc_love.rb</div>
+
+    <ul className={styles.editions}>
+      {editions.map(edition => (
+        <li key={edition}>
+          <a href={`https://${edition}.wrocloverb.com`}>{edition}</a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default withConfiguration(config => ({
+  editions: config.editions
+}))(Editions);
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import Responsive, { ResponsiveProvider } from '../components/contexts/responsive'
-import Head from '../components/head'
-import Header from '../components/header'
-import Logo from '../components/logo'
-import ConfrontIdeas from '../components/confront_ideas'
-import Postcard from '../components/postcard'
-import Agenda from '../components/agenda'
-import Speakers from '../components/speakers'
-import Location from '../components/location'
-import Partners from '../components/partners'
-import Supporters from '../components/supporters'
-import TalksArchive from '../components/talks_archive'
-import Twitter from '../components/twitter'
-import Footer from '../components/footer'
-import styles from './index.scss'
-import extendWithConferenceData from '../utils/extend_with_conference_data'
-import SocialMedia from "../components/footer/social_media"
-import { ConfigurationProvider } from '../components/contexts/configuration'
+import React from "react";
+import Responsive, {
+  ResponsiveProvider
+} from "../components/contexts/responsive";
+import Head from "../components/head";
+import Header from "../components/header";
+import Logo from "../components/logo";
+import ConfrontIdeas from "../components/confront_ideas";
+import Postcard from "../components/postcard";
+import Agenda from "../components/agenda";
+import Speakers from "../components/speakers";
+import Location from "../components/location";
+import Partners from "../components/partners";
+import Supporters from "../components/supporters";
+import TalksArchive from "../components/talks_archive";
+import Twitter from "../components/twitter";
+import Editions from "../components/editions";
+import Footer from "../components/footer";
+import styles from "./index.scss";
+import extendWithConferenceData from "../utils/extend_with_conference_data";
+import SocialMedia from "../components/footer/social_media";
+import { ConfigurationProvider } from "../components/contexts/configuration";
 
 class Home extends React.Component {
   render() {
@@ -24,10 +27,12 @@ class Home extends React.Component {
       <ResponsiveProvider>
         <ConfigurationProvider config={this.config()}>
           <div className={styles.body}>
-            <Head/>
-            <div className={`${styles.container} ${styles.container_stretched}`}>
+            <Head />
+            <div
+              className={`${styles.container} ${styles.container_stretched}`}
+            >
               <div className={styles.column1}>
-                <Logo/>
+                <Logo />
               </div>
               <div className={styles.column2}>
                 <Header />
@@ -43,12 +48,15 @@ class Home extends React.Component {
                 <Responsive mobile>
                   <Postcard />
                 </Responsive>
-                <ConfrontIdeas/>
+                <ConfrontIdeas />
                 <Agenda />
+                <Responsive desktop desktophd>
+                  <Editions />
+                </Responsive>
                 <Responsive tablet>
                   <Supporters />
                   <Partners />
-                  <Twitter/>
+                  <Twitter />
                 </Responsive>
               </div>
 
@@ -65,15 +73,19 @@ class Home extends React.Component {
                       <Location />
                       <Partners />
                       <Supporters />
-                      <Twitter/>
-                      <TalksArchive/>
+                      <Twitter />
+                      <TalksArchive />
+                      <Responsive mobile>
+                        <Editions />
+                      </Responsive>
                     </div>
                   </Responsive>
                   <Responsive tablet>
                     <div className={styles.content_column}>
                       <Speakers />
                       <Location />
-                      <TalksArchive/>
+                      <TalksArchive />
+                      <Editions />
                     </div>
                   </Responsive>
                 </div>
@@ -81,11 +93,11 @@ class Home extends React.Component {
             </div>
             <div className={styles.container}>
               <div className={styles.column1}>
-                <Footer/>
+                <Footer />
               </div>
               <div className={styles.column2}>
                 <div className={styles.footer}>
-                  <div className={styles.footer_empty}/>
+                  <div className={styles.footer_empty} />
                   <div className={styles.footer_sm}>
                     <SocialMedia />
                   </div>
@@ -95,8 +107,8 @@ class Home extends React.Component {
           </div>
         </ConfigurationProvider>
       </ResponsiveProvider>
-    )
+    );
   }
 }
 
-export default extendWithConferenceData(Home)
+export default extendWithConferenceData(Home);
