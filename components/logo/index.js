2ea7e1f345d4e496552e300e332ce2633987d70b
Michał Matyas <github@higher.lv>
2023-01-03 14:15:03 +0100

    Remove next/image

--- components/logo/index.js
+++ components/logo/index.js
@@ -2,11 +2,10 @@ import React from "react"
 import Link from "next/link"
 import logo from "./logo.png"
 import styles from "./index.module.scss"
-import Image from "next/image"
 
 const Logo = () => (
   <Link href="/" className={styles.logo}>
-    <Image src={logo} style={{ width: "100%", height: "auto" }} />
+    <img src={logo.src} />
   </Link>
 )
 
