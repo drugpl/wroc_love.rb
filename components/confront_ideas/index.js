cff082d0bcde4e1d55493d4ae3e99f5837a262a3
Michał Matyas <github@higher.lv>
2023-01-03 14:00:46 +0100

    Remove unnecessary paragraph separator characters

--- components/confront_ideas/index.js
+++ components/confront_ideas/index.js
@@ -33,14 +33,14 @@ const ConfrontIdeas = () => {
             inspire the way we write code, the way we help our clients and
             users. It’s not uncommon for us to present you ideas that come from
             the Java and .NET worlds, we believe that their ideas can have a
-            great influence on our community. There are good  reasons, why our
+            great influence on our community. There are good reasons, why our
             conference is called “the best Java conference in the Ruby world”.
           </p>
           <p>
-            Our goal is to ensure a good  environment to learn and discuss. We
+            Our goal is to ensure a good environment to learn and discuss. We
             love confronting ideas, even when it’s uncomfortable to the Ruby
-             status-quo. We love experimenting with new formats of talks and
-             discussions, like fights, fishbowls and crowd-moderated discussion
+            status-quo. We love experimenting with new formats of talks and
+            discussions, like fights, fishbowls and crowd-moderated discussion
             panels. Everything that can inspire the Ruby programmers is more
             than welcome at our conference.{" "}
             <a href="mailto:all@wrocloverb.com">Let us know</a>, what you’d be
